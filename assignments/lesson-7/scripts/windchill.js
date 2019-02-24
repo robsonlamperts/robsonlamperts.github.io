b5a611503a1fb2cfc5170a253736f297db7be727
robsonlamperts <Iam16009@byui.edu>
2019-02-23 22:07:49 -0300

    week 7

---
+++ assignments/lesson-7/scripts/windchill.js
@@ -0,0 +1,9 @@
+// Input - get input and convert that string to an integer
+var x = parseInt(document.getElementById('temp').innerHTML);
+var y = parseInt(document.getElementById('speed').innerHTML);
+
+// Processing - some random formula processing with the variable
+var result = 35.74 + 0.6215 * x - 35.75 * Math.pow(y, 0.16) + 0.4275 * x * Math.pow(y, 0.16);
+
+// Output - round to one decimal and output string with HTML to innerHTML of a div
+document.getElementById('output').innerHTML = "Wind Chill: " + result.toFixed(1) + "°F";
