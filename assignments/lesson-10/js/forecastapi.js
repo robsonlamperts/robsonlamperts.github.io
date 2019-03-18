91da257231efa187aacd7cd2a925629d71e42b42
robsonlamperts <Iam16009@byui.edu>
2019-03-18 14:04:25 -0300

    https

--- assignments/lesson-10/js/forecastapi.js
+++ assignments/lesson-10/js/forecastapi.js
@@ -1,6 +1,6 @@
 let forecastRequest = new XMLHttpRequest();
 
-let apiURLstring = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=02933daf7eacd95eb67507a4e55f6361';
+let apiURLstring = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=02933daf7eacd95eb67507a4e55f6361';
 forecastRequest.open('Get', apiURLstring, true);
 forecastRequest.send();
 
